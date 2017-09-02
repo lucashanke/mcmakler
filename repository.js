2b293585822bbe79df7af30a936efa368b0c3275
Lucas Hanke <lhanke@thoughtworks.com>
2017-09-02 14:33:58 -0300

    change persist method to save many

--- repository.js
+++ repository.js
@@ -9,7 +9,8 @@ mongoose.Promise = global.Promise;
 
 mongoose.connect(MONGO_DB_URL, { useMongoClient: true });
 
-export const persistNEO = (neo) => neo.save().catch(err =>  console.log(err.message));
+export const persistNEOs = (neos, callback) =>
+  NEO.insertMany(neos, callback).catch(err =>  console.log(err.message));
 
 export const fetchHazardous = (callback) =>
   NEO.find({ isHazardous: true }, callback);
