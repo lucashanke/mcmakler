db5ec167631b2f0201a973fe99dce26a82ff228d
Lucas Hanke <lhanke@thoughtworks.com>
2017-09-02 15:21:50 -0300

    add neo routes to index

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from 'express';
-import root, { hazardous } from './routes.js';
+import root, { hazardous, fastest, bestYear, bestMonth } from './routes.js';
 
 const app = express();
 
 app.get('/', root);
-app.get('/hazardous', hazardous);
+app.get('/neo/hazardous', hazardous);
+app.get('/neo/fastest', fastest);
+app.get('/neo/best-year', bestYear);
+app.get('/neo/best-month', bestMonth);
 
 app.listen(3001);
