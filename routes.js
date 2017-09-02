ec4616206c1a3f6c9967eedbf3e2b792f24701d9
Lucas Hanke <lhanke@thoughtworks.com>
2017-09-02 15:21:31 -0300

    add routes for fastest, best year/month

--- routes.js
+++ routes.js
@@ -1,6 +1,15 @@
-import { fetchHazardous } from './repository.js';
+import { fetchHazardous, fetchFastest, fetchBestYear, fetchBestMonth } from './repository.js';
 
 const root = (req, res) => res.json({"hello":"world!"});
 export const hazardous = (req, res) => fetchHazardous((err, neos) => res.json(neos));
+
+export const fastest = (req, res) =>
+  fetchFastest((err, neo) => res.json(neo), req.query.hazardous === 'true' || false);
+
+export const bestYear = (req, res) =>
+  fetchBestYear((err, year) => res.json(year), req.query.hazardous === 'true' || false);
+
+export const bestMonth = (req, res) =>
+  fetchBestMonth((err, month) => res.json(month), req.query.hazardous === 'true' || false);
 
 export default root;
