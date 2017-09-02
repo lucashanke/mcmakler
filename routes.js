218f5ff466f4d3a63c582d5c97959fe398d2502a
Lucas Hanke <lhanke@thoughtworks.com>
2017-09-02 17:59:55 -0300

    add error handling on routes

--- routes.js
+++ routes.js
@@ -1,15 +1,37 @@
 import { fetchHazardous, fetchFastest, fetchBestYear, fetchBestMonth } from './repository.js';
 
+const NO_NEOS_FOUND = 'No NEOs found.';
+const ERROR_MESSAGE = 'Sorry, but something went wrong.';
+
 const root = (req, res) => res.json({"hello":"world!"});
+
 export const hazardous = (req, res) => fetchHazardous((err, neos) => res.json(neos));
 
 export const fastest = (req, res) =>
-  fetchFastest((err, neo) => res.json(neo), req.query.hazardous === 'true' || false);
+  fetchFastest((err, neo) => {
+    if (err === null) {
+      if (neo.length === 0) return res.status(404).json({ message: NO_NEOS_FOUND});
+      return res.json(neo[0]);
+    }
+    return res.status(500).send({ message: ERROR_MESSAGE});
+  }, req.query.hazardous === 'true' || false);
 
 export const bestYear = (req, res) =>
-  fetchBestYear((err, year) => res.json(year), req.query.hazardous === 'true' || false);
+  fetchBestYear((err, year) => {
+  if (err === null) {
+    if (year.length === 0) return res.status(404).json({ message: NO_NEOS_FOUND});
+      return res.json(year[0]);
+    }
+    return res.status(500).send({ message: ERROR_MESSAGE});
+  }, req.query.hazardous === 'true' || false);
 
 export const bestMonth = (req, res) =>
-  fetchBestMonth((err, month) => res.json(month), req.query.hazardous === 'true' || false);
+  fetchBestMonth((err, month) => {
+    if (err === null) {
+      if (month.length === 0) return res.status(404).json({ message: NO_NEOS_FOUND});
+      return res.json(month[0]);
+    }
+    return res.status(500).send({ message: ERROR_MESSAGE});
+  }, req.query.hazardous === 'true' || false);
 
 export default root;
